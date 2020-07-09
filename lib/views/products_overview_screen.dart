771ec8b51527111b3e91ec20da44cf8ee753736c
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-09 15:50:48 -0300

    Creating ProductItem component

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../data/dummy_data.dart';
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = DUMMY_PRODUCTS;
@@ -17,11 +18,11 @@ class ProductsOverviewScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 2 / 3,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
+        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
       ),
     );
   }
