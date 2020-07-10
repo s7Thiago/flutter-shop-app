f26bffda558a1185d6b26a2bebfa013be4b8c126
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-10 09:48:47 -0300

    Alowing that favorite property from items to be controled globally

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions { Favorites, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('My Store'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedOption) {
+              if (selectedOption == FilterOptions.Favorites) {
+                products.showFavoriteOnly();
+              } else {
+                products.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Favorites only'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
       body: ProductGrid(),
     );
