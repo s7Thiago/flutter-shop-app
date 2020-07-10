b06215e74083e1b65aaef2bdf57e4a99b49f8e02
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-09 21:57:05 -0300

    Undoing last changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,23 @@ import 'package:flutter/material.dart';
 import './views/products_overview_screen.dart';
 import 'utils/routes.dart';
 import './views/product_datail_screen.dart';
-import './providers/counter_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        title: 'My Store',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        routes: {
-          AppRoutes.HOME: (_) => ProductsOverviewScreen(),
-          AppRoutes.PRODUCT_DETAIL: (_) => ProductDetatilScreen(),
-        },
+    return MaterialApp(
+      title: 'My Store',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
+      routes: {
+        AppRoutes.HOME: (_) => ProductsOverviewScreen(),
+        AppRoutes.PRODUCT_DETAIL: (_) => ProductDetatilScreen(),
+      },
     );
   }
 }
--- lib/views/product_datail_screen.dart
+++ lib/views/product_datail_screen.dart
@@ -1,42 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../models/product.dart';
-import '../providers/counter_provider.dart';
 
-class ProductDetatilScreen extends StatefulWidget {
-  @override
-  _ProductDetatilScreenState createState() => _ProductDetatilScreenState();
-}
-
-class _ProductDetatilScreenState extends State<ProductDetatilScreen> {
+class ProductDetatilScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CounterProvider counter = CounterProvider.of(context);
-
     final Product product =
         ModalRoute.of(context).settings.arguments as Product;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              onPressed: () {
-                setState(() {
-                  counter.state.inc();
-                });
-                print('Provider: ${counter.state.value}');
-              },
-              child: Icon(Icons.add),
-            ),
-            Text(counter.state.value.toString())
-          ],
-        ),
       ),
     );
   }
