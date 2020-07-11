835d896137e99206018fdb28a714d3e42a2e1655
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-11 14:05:44 -0300

    melhorias e adições na tela de pedidos

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../widgets/app_drawer.dart';
+import '../widgets/order_widget.dart';
+
+import '../providers/orders.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final orders = Provider.of<Orders>(context);
+
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: Text('My Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx, index) => OrderWidget(orders.items[index]),
       ),
     );
   }
