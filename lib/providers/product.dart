b4f66435c9cb8d9a0203a7693cb74a601885054a
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-23 17:51:35 -0300

    Making the toggle favorite feature reflect on firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop_app/exceptions/http_exception.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +21,30 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    final url =
+        'https://flutter-cod3r-shop-68ee0.firebaseio.com/products/$id.json';
+
+    _toggleFavorite();
+
+    try {
+      var response = await http.patch(
+        url,
+        body: json.encode(
+          {'isFavorite': isFavorite},
+        ),
+      );
+
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } on HttpException catch (error) {
+      _toggleFavorite();
+    }
   }
 }
