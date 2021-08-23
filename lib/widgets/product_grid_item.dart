8992f7fdc66987088f49f1e22273378c6c4a8488
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-23 11:23:03 -0300

    Adding hero animation when user got to product details

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -24,10 +24,14 @@ class ProductGridItem extends StatelessWidget {
                 arguments: product,
               );
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
