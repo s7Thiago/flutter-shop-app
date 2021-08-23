912c68983f99279c4918aa96c70a9bbb686d64c7
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-23 11:19:16 -0300

    Added fading placeholder animation on product image while the product image is not loaded from network

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -18,17 +18,17 @@ class ProductGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              AppRoutes.PRODUCT_DETAIL,
-              arguments: product,
-            );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.PRODUCT_DETAIL,
+                arguments: product,
+              );
+            },
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: Consumer<Product>(
